14ff832ceac58b5003d941dd63d6be2c2bd1f980
Hioshizuka <Hioshizuka@users.noreply.github.com>
2017-11-21 00:49:06 -0500

    ver2.1

--- Background/main.cpp
+++ Background/main.cpp
@@ -22,42 +22,18 @@
 
 const double PI = 3.1415927;
 
-inline void draw_sth(int x, int y, GLuint texId, int wid, int hei)
-{
-	glBindTexture(GL_TEXTURE_2D, texId);
-	glBegin(GL_QUADS);
-
-	glTexCoord2d(0.0, 0.0);
-	glVertex2i(x, y);
-
-	glTexCoord2d(1.0, 0.0);
-	glVertex2i(x + wid, y);
-
-	glTexCoord2d(1.0, 1.0);
-	glVertex2i(x + wid, y + hei);
-
-	glTexCoord2d(0.0, 1.0);
-	glVertex2i(x, y + hei);
-
-	glEnd();
-}
-
 int main(void)
 {    
 	// set camera
 	int camera_x = 0;
 
 	// initialization
-
-	////////////////////////////////////////////
 	CharBitmap bmp;
 	bmp.init();
 	
-	// texture parameters
 	Texture TX;
 	TX.init();
 	
-	////////////////////////////////////////////////
 
 	FsOpenWindow(16, 16, 768, 576, 1);
 	TX.set();
@@ -84,14 +60,13 @@ int main(void)
 		}
 
 		/////////////////////// draw everything ////////////////////
-	    // draw blue sky
+	    // draw background
 		bmp.Draw_bgd();
 
-		////////////////////////////////////////////////
-		// texture map setting begins here 
+		// texture mapping goes here 
 		TX.Draw_texture(camera_x);
 
-		// Bitmap drawing goes here//////
+		// Bitmap drawing goes here
 		bmp.Draw_everything(camera_x);
 		
 
